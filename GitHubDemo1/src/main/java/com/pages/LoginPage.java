173f069c11b70f829367f4d6edc64f5c89433e77
LENOVO <LENOVO@DESKTOP-QC3JFTU>
2022-07-26 16:50:07 +0530

    First commit

---
+++ GitHubDemo1/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class LoginPage {
+	
+	public void loginPage() {
+		System.out.println("This is my Login Page");
+		
+		System.out.println("This is eclipse git demo");
+	}
+
+}
---
+++ GitHubDemo1/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,9 @@
+package com.testcases;
+
+public class LoginTest {
+	
+	public void loginetest() {
+		System.out.println("This is my LoginTest to check get through eclipse");
+	}
+
+}
